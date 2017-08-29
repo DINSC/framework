10d3006391b38a312946b72f1897c3008777cdc8
Cyril POIDEVIN <vilsafur@gmail.com>
2017-08-29 23:53:40 +0200

    Fix psr2 errors

--- spec/HttpKernelSpec.php
+++ spec/HttpKernelSpec.php
@@ -4,10 +4,8 @@ use Kyffoo\Kernel\HttpKernel;
 use Psr\Http\Message\ResponseInterface;
 
 describe(HttpKernel::class, function () {
-
     describe('::make', function () {
-
-        it('should return a HttpResponse', function() {
+        it('should return a HttpResponse', function () {
             $kernel = new HttpKernel('dev', true);
             $response = $kernel->make();
             expect($response)->toBeAnInstanceOf(ResponseInterface::class);
